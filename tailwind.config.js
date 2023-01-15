26b151b62fe5d61d36e04b2f99d6e41452d4cb73
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-01-16 00:59:28 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+}
